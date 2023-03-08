e4dd6c020fbf4d7761210995a9dfb815d41c5221
k-nagama <k.nagama0632@gmail.com>
2023-03-08 15:25:47 +0900

    typescript コードに変換

--- src/easy-storage.js
+++ src/easy-storage.js
@@ -1,100 +1,106 @@
-class EasyStorage
-{
-  static emptyKey = 'キーは必須です';
-
-  /**
-   * @param {string} storageType
-   */
-  constructor(storageType)
-  {
-    if (!storageType) {
-      storageType = 'local';
+/**
+ * @class
+ */
+var EasyStorage = /** @class */ (function () {
+    /**
+     * @constructor
+     *
+     * @param {string} storageType
+     */
+    function EasyStorage(storageType) {
+        if (!storageType) {
+            storageType = 'local';
+        }
+        switch (storageType) {
+            case 'local':
+                this.storage = localStorage;
+                break;
+            case 'session':
+                this.storage = sessionStorage;
+                break;
+            default:
+                throw new Error('種別は local もしくは session で指定してください');
+        }
     }
-
-    switch (storageType) {
-      case 'local':
-        this.storage = localStorage;
-        break;
-      case 'session':
-        this.storage = sessionStorage;
-        break;
-      default:
-        throw new Error('種別は local もしくは session で指定してください');
-    }
-  }
-
-  /**
-   *
-   * @param {string} key
-   * @param {Object|string} value
-   */
-  setItem(key, value)
-  {
-    if (!key) {
-      throw new Error(EasyStorage.emptyKey);
-    }
-
-    // value がオブジェクトである場合、JSON.stringify() を使って文字列に変換します
-    if (typeof value === 'object') {
-      value = JSON.stringify(value);
-    }
-
-    this.storage.setItem(key, value);
-  }
-
-  /**
-   * @param {string} key
-   */
-  getItem(key)
-  {
-    if (!key) {
-      throw new Error(EasyStorage.emptyKey);
-    }
-
-    const value = this.storage.getItem(key);
-    if (value == null) {
-      return null;
-    }
-
-    try {
-      return JSON.parse(value);
-    } catch (error) {
-      return value;
-    }
-  }
-
-  /**
-   * @param {string} key
-   */
-  removeItem(key)
-  {
-    if (!key) {
-      throw new Error(EasyStorage.emptyKey);
-    }
-
-    this.storage.removeItem(key);
-  }
-
-  /**
-   *
-   */
-  clear()
-  {
-    this.storage.clear();
-  }
-
-  /**
-   * @param {string} key
-   * @returns {boolean}
-   */
-  hasItem(key)
-  {
-    if (!key) {
-      throw new Error(EasyStorage.emptyKey);
-    }
-
-    return this.storage.getItem(key) !== null;
-  }
-}
-
+    /**
+     * @method
+     *
+     * @param {string} key
+     * @param {Object|string} value
+     */
+    EasyStorage.prototype.setItem = function (key, value) {
+        if (!key) {
+            throw new Error(EasyStorage.emptyKey);
+        }
+        // value がオブジェクトである場合、JSON.stringify() を使って文字列に変換します
+        if (typeof value === 'object') {
+            this.storage.setItem(key, JSON.stringify(value));
+        }
+        else if (typeof value === 'string') {
+            this.storage.setItem(key, value);
+        }
+    };
+    /**
+     * @method
+     *
+     * @param {string} key
+     * @returns {string|number|null}
+     */
+    EasyStorage.prototype.getItem = function (key) {
+        if (!key) {
+            throw new Error(EasyStorage.emptyKey);
+        }
+        var value = this.storage.getItem(key);
+        if (value == null) {
+            return null;
+        }
+        try {
+            return JSON.parse(value);
+        }
+        catch (error) {
+            return value;
+        }
+    };
+    /**
+     * @method
+     *
+     * @param {string} key
+     * @returns {void}
+     *
+     * @throws {Error} keyが指定されていない場合
+     */
+    EasyStorage.prototype.removeItem = function (key) {
+        if (!key) {
+            throw new Error(EasyStorage.emptyKey);
+        }
+        this.storage.removeItem(key);
+    };
+    /**
+     * @method
+     *
+     * @returns {void}
+     */
+    EasyStorage.prototype.clear = function () {
+        this.storage.clear();
+    };
+    /**
+     * @method
+     *
+     * @param {string} key
+     * @returns {boolean}
+     *
+     * @throws {Error} keyが指定されていない場合
+     */
+    EasyStorage.prototype.hasItem = function (key) {
+        if (!key) {
+            throw new Error(EasyStorage.emptyKey);
+        }
+        return this.storage.getItem(key) !== null;
+    };
+    /**
+     * @type {string}
+     */
+    EasyStorage.emptyKey = 'キーは必須です';
+    return EasyStorage;
+}());
 module.exports = EasyStorage;
